db7bf137a8ddb76a5e760b39db15b7f7b6e0b1a0
Alexandr5522 <wodol604@gmail.com>
2024-10-02 20:51:16 +0300

    30/09_2

---
+++ module_1_2var..py
@@ -0,0 +1,6 @@
+completed_homework=12
+hours_spent=1.5
+course_name='Python'
+print(hours_spent/completed_homework)  #time_fork_one_task
+time_fork_one_task=0.125
+print ('Курс:',course_name,',всего часов:',completed_homework,',затрачено часов:',hours_spent,',среднее время выполнения:',time_fork_one_task,'часа')
